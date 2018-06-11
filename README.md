4cfed55268c673732bf18970aa57ba53409b927b
Mariano Koldobsky <mariano.koldobsky@mercadolibre.com>
2018-06-11 15:38:22 -0300

    update ejercicio

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 
 1. Input
 
-```1
+```
+1
 3
 0606142000 0606142200
 0606144000 0606145000
@@ -10,7 +11,9 @@
 ```
 
 2. Output
+```
 1
+```
 
 3. Explicación Input
 - primera linea: cantidad actual de telemarketers
@@ -21,20 +24,6 @@
 - si hay llamadas que no puede atender un telemarketer dice cuantos se necesitan
 - Si puede atender todas responde 0
 
-
-## Pasos para realizar el workshop
-
-1. Clonamos el repo:
-
-    ```bash
-        git clone https://github.com/diegosanchez/dojo.git
-    ```
-
-2. Descargamos el branch remote correspondiente al workshop:
-
-    ```bash
-        git checkout workshop
-    ```
 
 ### Pasos específicos para javascript
 
@@ -60,26 +49,3 @@
 
 2. node 4.2.3
 3. npm  2.14.7
-
-
-### Pasos específicos para java
-
-1. Ejecutamos los tests
-
-    ```bash
-        mvn test
-        ( Estando en la carpeta que contiene el pom.xml )
-    ```
-
-
-
-#### Qué necesito?
-
-1. Maven 3+ ( brew install maven )
-2. JDK 1.7 u 1.8## Pasos para realizar el workshop
-
-
-## Qué necesito?
-
-1. Notebook para realizar workshop (si queres hacerlo, sino podes venir a compartir tus experiencias)
-2. Ganas de compartir tu solución.
--- test/solution.js
+++ test/solution.js
@@ -1,20 +1,20 @@
 const chai = require('chai');
-const SpreadSheet = require('../spreadSheet.js');
+//const SpreadSheet = require('../spreadSheet.js');
 
-var sheet;
+//var sheet;
 
 describe("solution", () => {
     beforeEach(() => {
-        sheet = new SpreadSheet();
+        //sheet = new SpreadSheet();
     });
 
     describe("sheet", () => {
         describe("cells", () => {
             it.skip("should populate two cells with numeric values and response the stored values", () => {
 
-                // ... 
+                // ...
                 // Code here!
-                // ... 
+                // ...
 
                 chai.expect( sheet.get("a1") )
                     .to.be.eql(1);
@@ -24,9 +24,9 @@ describe("solution", () => {
 
             it.skip("should polulate cells with a label and a value and response the stored values", () => {
 
-                // ... 
+                // ...
                 // Code here!
-                // ... 
+                // ...
 
                 chai.expect( sheet.get("a1") )
                     .to.be.eql("Valor:");
@@ -36,9 +36,9 @@ describe("solution", () => {
 
             it.skip("should store values and update values references", () => {
 
-                // ... 
+                // ...
                 // Code here!
-                // ... 
+                // ...
 
                 chai.expect( sheet.get("a1") )
                     .to.be.eql(1);
@@ -48,9 +48,9 @@ describe("solution", () => {
 
             it.skip("should response summatory", () => {
 
-                // ... 
+                // ...
                 // Code here!
-                // ... 
+                // ...
 
                 chai.expect( sheet.get("a3") )
                     .to.be.eql(3);
@@ -63,9 +63,9 @@ describe("solution", () => {
 
             it.skip("should response summatory - broken range", () => {
 
-                // ... 
+                // ...
                 // Code here!
-                // ... 
+                // ...
 
                 chai.expect( sheet.get("a3") )
                     .to.be.eql(3);
